f4f0eeb357dc049258b22bde10f80921260c0812
Aschkor <aschkor@protonmail.com>
2022-02-20 16:58:33 +0100

    Change init.vim to init.lua

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Each plugin has its configuration file in ./lua/config
+
+-- Basic options for neovim
+require('config.options')
+
+-- Keybinding for neovim
+require('config.keybinding')
+
+-- Configuration for the plugin manager
+require('config.plugins_manager')
