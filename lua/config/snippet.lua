93102b9228de37779431f2f6de0a65341dc93ba8
Aschkor <aschkor@protonmail.com>
2022-03-20 23:00:22 +0100

    Add snippet for python

--- lua/config/snippet.lua
+++ lua/config/snippet.lua
@@ -1,16 +1,22 @@
 local snippet = {}
 local snip = require('luasnip')
 
+local t = function(str)
+		return vim.api.nvim_replace_termcodes(str, true, true, true)
+	end
+
+
+
 _G.expand_or_jump_ifn = function()
 		if snip.expand_or_jumpable() then
-			snip.expand_or_jump()
+			return t('<cmd>lua require("luasnip").expand_or_jump()<CR>')
 		end
 		return ''
 	end
 
 _G.jump_back_ifn = function() 
 		if snip.jumpable(-1) then
-			snip.jump(-1)
+			return t('<cmd>lua require("luasnip").jump(-1)<CR>')
 		end
 		return ''
 	end
@@ -20,6 +26,13 @@ function snippet.config()
 	vim.api.nvim_set_keymap('s', '<c-l>', 'v:lua.expand_or_jump_ifn()', {expr= true})
 	vim.api.nvim_set_keymap('i', '<c-h>', 'v:lua.jump_back_ifn()', {expr = true})
 	vim.api.nvim_set_keymap('s', '<c-h>', 'v:lua.jump_back_ifn()', {expr = true})
+
+	snip.snippets = {
+		python = {
+			snip.parser.parse_snippet("cl", "class ${1}:\n	${0}"),
+			snip.parser.parse_snippet("def", "def ${1}(${2}):\n	$0"),
+		}
+	}
 end
 
 return snippet
