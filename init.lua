725e0e6b52f42c7a573c8b379b739e6853bce75b
Aschkor <aschkor@protonmail.com>
2024-03-02 17:58:22 +0100

    Add local config

--- init.lua
+++ init.lua
@@ -1,24 +1,42 @@
 -- Each plugin has its configuration file in ./lua/config
 -- Basic options for neovim
-require('config.options')
 
+local set = vim.opt
+
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = false
+set.scrolloff = 7
+set.nu = true
+set.autoread = true
+set.rnu = true
+set.cursorline = true
+
+vim.filetype.add {
+	extension = {
+		qml = 'qmljs',
+	},
+}
 -- Keybinding for neovim
-require('config.keybinding')
-
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('n', 'ZW', '<cmd>wall<CR>', { noremap = true })
 -- Configuration for the plugin manager
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+	vim.fn.system {
 		'git',
 		'clone',
 		'--filter=blob:none',
 		'https://github.com/folke/lazy.nvim.git',
 		'--branch=stable',
-		lazypath
-	})
+		lazypath,
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup 'plugins'
 
-require('config.commands')
+if nil ~= vim.loop.fs_stat(vim.fn.stdpath 'config' .. '/local_config.lua') then
+	require 'local_config'
+end
