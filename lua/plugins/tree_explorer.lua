e117b6ea69b2063a5cb0d1850c31afe32caff530
Aschkor <aschkor@protonmail.com>
2024-10-23 22:12:44 +0200

    Add option to nvim-tree

--- lua/plugins/tree_explorer.lua
+++ lua/plugins/tree_explorer.lua
@@ -4,19 +4,41 @@ return {
 		'nvim-tree/nvim-tree.lua',
 		keys = '<F3>',
 		depedency = 'nvim-tree/nvim-web-devicons',
-		config = function ()
+		config = function()
 			vim.g.loaded_netrw = 1
 			vim.g.loaded_netrwPlugin = 1
-			require('nvim-tree').setup{
+			require('nvim-tree').setup {
 				on_attach = function(bufnr)
-					local api = require'nvim-tree.api'
+					local api = require 'nvim-tree.api'
 					api.config.mappings.default_on_attach(bufnr)
-					vim.keymap.set('n','<C-s>', api.node.open.horizontal, {desc = 'nvim-tree: Open Horizontal Split', buffer = bufnr, noremap = true, silent = true, nowait = true})
+					vim.keymap.set('n', '<C-s>', api.node.open.horizontal, {
+						desc = 'nvim-tree: Open Horizontal Split',
+						buffer = bufnr,
+						noremap = true,
+						silent = true,
+						nowait = true,
+					})
 				end,
-				git = {enable = false}
+				git = { enable = false },
+				renderer = {
+					full_name = true,
+				},
+				view = {
+					centralize_selection = true,
+				},
+				tab = {
+					sync = {
+						open = true,
+						close = true,
+					},
+				},
 			}
-			vim.api.nvim_set_keymap( 'n', '<F3>','<cmd>lua require("nvim-tree.api").tree.toggle()<CR>', {noremap=true})
-		end
-	}
+			vim.api.nvim_set_keymap(
+				'n',
+				'<F3>',
+				'<cmd>lua require("nvim-tree.api").tree.toggle()<CR>',
+				{ noremap = true }
+			)
+		end,
+	},
 }
-
