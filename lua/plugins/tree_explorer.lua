70a2253df843c346198b55f2b5014f76a4314587
Aschkor <aschkor@protonmail.com>
2024-11-30 19:44:02 +0100

    Change name tab when open a new one with nvim-tree

--- lua/plugins/tree_explorer.lua
+++ lua/plugins/tree_explorer.lua
@@ -20,10 +20,19 @@ return {
 					end
 					local ok, api = pcall(require, 'nvim-tree.api')
 					assert(ok, 'api module is not found')
+					api.config.mappings.default_on_attach(bufnr)
 					vim.keymap.set('n', '<CR>', api.node.open.tab_drop, opts 'Tab drop')
 
-					local api = require 'nvim-tree.api'
-					api.config.mappings.default_on_attach(bufnr)
+					vim.keymap.set('n', '<C-t>', function()
+						local node = api.tree.get_node_under_cursor()
+						local tree_win = api.tree.winid()
+						for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+							if win ~= vim.fn.bufwinid(tree_win) then
+								vim.api.nvim_set_current_win(win)
+							end
+						end
+						api.node.open.tab(node)
+					end, opts 'Open: New Tab')
 					vim.keymap.set('n', '<C-s>', api.node.open.horizontal, {
 						desc = 'nvim-tree: Open Horizontal Split',
 						buffer = bufnr,
